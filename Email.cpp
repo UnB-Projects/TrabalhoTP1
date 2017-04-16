db631c9457657bfbaf1224cb8ea6084523e74b3e
igorfg <igorfgds@gmail.com>
2017-04-16 17:43:34 -0300

    Atributos do Email alterados junto com sua funcao de validacao

--- Email.cpp
+++ Email.cpp
@@ -13,8 +13,7 @@ void Email::valida(string email) throw (invalid_argument) {
 	int i = 0;
 	string usuario, provedor, extensao;
 	string verifica;
-	Nome nomeUsuario, nomeProvedor, nomeExtensao;
-
+	
 	while (i < email.size()) {
 		if (email[i] == '@') {
 			verifica.push_back('@');
@@ -22,35 +21,15 @@ void Email::valida(string email) throw (invalid_argument) {
 		else if (email[i] == '.') {
 			verifica.push_back('.');
 		}
+
+		else if ((email[i] < 'A' || email[i] > 'Z') && (email[i] < 'a' || email[i] > 'z')) {
+			throw invalid_argument ("O email deve ser no formato L@L.L, onde L sao letras.");	
+		}
+
 		i++;
 	}
 
 	if (verifica.compare("@.") != 0) {
 		throw invalid_argument ("O email deve ser no formato L@L.L, onde L sao letras.");
 	}
-
-	i = 0;
-
-	while (email[i] != '@') {
-		usuario.push_back(email[i]);
-		i++;
-	}
-
-	nomeUsuario.setNome(usuario);
-
-	i++;
-	while (email[i] != '.') {
-		provedor.push_back(email[i]);
-		i++;
-	}
-
-	nomeProvedor.setNome(provedor);
-
-	i++;
-	while (i < email.size()) {
-		extensao.push_back(email[i]);
-		i++;
-	}
-
-	nomeExtensao.setNome(extensao);
 }
